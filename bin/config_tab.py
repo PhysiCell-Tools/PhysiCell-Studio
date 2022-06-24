6e5c100524baf78c1e4bf1df19f925509039ebb0
Randy Heiland <randy.heiland@gmail.com>
2022-06-24 07:21:00 -0400

    csv folder, 3D check

--- bin/config_tab.py
+++ bin/config_tab.py
@@ -3,15 +3,13 @@ Authors:
 Randy Heiland (heiland@iu.edu)
 Adam Morrow, Grant Waldrow, Drew Willis, Kim Crevecoeur
 Dr. Paul Macklin (macklinp@iu.edu)
-
---- Versions ---
-0.1 - initial version
 """
 
 import sys
 # import xml.etree.ElementTree as ET  # https://docs.python.org/2/library/xml.etree.elementtree.html
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QFrame,QApplication,QWidget,QTabWidget,QLineEdit, QVBoxLayout,QRadioButton,QLabel,QCheckBox,QComboBox,QScrollArea,QGridLayout
+from PyQt5.QtWidgets import QMessageBox
 
 class QHLine(QFrame):
     def __init__(self):
@@ -431,6 +429,24 @@ class Config(QWidget):
         self.xml_root.find(".//z_max").text = self.zmax.text()
         self.xml_root.find(".//dz").text = self.zdel.text()
 
+        # is this model 2D or 3D?
+        zmax = float(self.zmax.text())
+        zmin = float(self.zmin.text())
+        zdel = float(self.zdel.text())
+        if (zmax-zmin) > zdel:
+            self.xml_root.find(".//domain//use_2D").text = 'false'
+        else:
+            self.xml_root.find(".//domain//use_2D").text = 'true'
+        
+        # may want to check for (max-min) being an integer multiple of delta spacings:
+            # msg = QMessageBox()
+            # msg.setIcon(QMessageBox.Critical)
+            # msg.setText("Warning")
+            # msg.setInformativeText('The output intervals for SVG and full (in Config Basics) do not match.')
+            # msg.setWindowTitle("Warning")
+            # msg.exec_()
+
+
         # if not self.xml_root.find(".//virtual_wall_at_domain_edge"):
         # opts = self.xml_root.find(".//options")
         # if not opts:
@@ -504,7 +520,6 @@ class Config(QWidget):
         else:
             self.xml_root.find(".//initial_conditions//cell_positions").attrib['enabled'] = 'false'
 
-        # self.xml_root.find(".//initial_conditions//cell_positions/folder").text = ''
         # self.xml_root.find(".//initial_conditions//cell_positions/folder").text = './data'
         self.xml_root.find(".//initial_conditions//cell_positions/folder").text = self.csv_folder.text()
         print("------- config_tab.py: fill_xml(): setting csv folder = ",self.csv_folder.text())
