e4d9a6728c8921d102c1967c8f7bfecce7772adf
Randy Heiland <randy.heiland@gmail.com>
2023-03-29 11:55:49 -0400

    fix missing files

---
+++ data/configs_v1.11.0/copy.sh
@@ -0,0 +1,19 @@
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/biorobots/config/PhysiCell_settings.xml biorobots.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/cancer_biorobots/config/PhysiCell_settings.xml cancer_biorobots.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/cancer_immune/config/PhysiCell_settings.xml cancer_immune.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/celltypes3/config/PhysiCell_settings.xml celltypes3.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/heterogeneity/config/PhysiCell_settings.xml heterogeneity.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/interactions/config/PhysiCell_settings.xml interactions.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/interactions/config/PhysiCell_settings-with-substrate-in-SVG.xml interactions_substrate_in_svg.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/mechano/config/PhysiCell_settings.xml mechano.xml
+
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/mechano/config/*.csv .
+mv cells.csv mechano.csv   # then edit mechano.xml
+
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/pred_prey_farmer/config/PhysiCell_settings.xml pred_prey_farmer.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/template/config/PhysiCell_settings.xml template.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/virus_macrophage/config/PhysiCell_settings.xml virus_macrophage.xml
+
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/worm/config/PhysiCell_settings.xml worm.xml
+cp ~/git/PhysiCell_rheiland_dev/sample_projects/worm/config/cells.csv worm.csv  # and edit the .xml
+
