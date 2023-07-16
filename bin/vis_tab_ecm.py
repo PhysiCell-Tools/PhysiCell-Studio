c76d3220f7342bf6a77ddc153461d2fcde137e6e
Randy Heiland <randy.heiland@gmail.com>
2023-07-16 05:46:21 -0400

    fix frame advance

--- bin/vis_tab_ecm.py
+++ bin/vis_tab_ecm.py
@@ -256,15 +256,15 @@ class Vis(VisBase, QWidget):
         #     print(line.strip())
         self.ax0.cla()
         if self.substrates_checked_flag:  # do first so cells are plotted on top
-            self.plot_substrate(self.current_svg_frame)
+            self.plot_substrate(self.current_frame)
         if self.cells_checked_flag:
             if self.plot_cells_svg:
-                self.plot_svg(self.current_svg_frame)
+                self.plot_svg(self.current_frame)
             else:
-                self.plot_cell_scalar(self.current_svg_frame)
+                self.plot_cell_scalar(self.current_frame)
 
         self.called_from_update = True
-        self.frame_count.setText(str(self.current_svg_frame))
+        self.frame_count.setText(str(self.current_frame))
         self.called_from_update = False
 
         self.canvas.update()
