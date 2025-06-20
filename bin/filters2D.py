6b8c17a8b0161397699e681ff2506a6ea16897ef
Daniel Bergman <danielrbergman@gmail.com>
2025-06-20 16:52:56 -0700

    also plot graph for neighbors and attachments
    and do it on svgs, too.
    
    known issue: if filtering on cell types, I think this will still show graph edges for cells filtered out.

--- bin/filters2D.py
+++ bin/filters2D.py
@@ -75,10 +75,11 @@ class FilterUI2DWindow(QWidget):
         idx_row += 1
         glayout.addWidget(QHLine(), idx_row,0,1,4) # w, row, column, rowspan, colspan
         idx_row += 1
-        self.attachments_checkbox = QCheckBox_custom('attachments')
-        self.attachments_checkbox.setChecked(self.vis_tab.attachments_checked_flag)
-        self.attachments_checkbox.clicked.connect(self.attachments_toggle_cb)
-        glayout.addWidget(self.attachments_checkbox)
+        self.graph_display_combobox = QComboBox()
+        self.graph_display_combobox.addItems(['NONE', 'neighbors', 'attachments', 'spring attachments'])
+        self.graph_display_combobox.currentIndexChanged.connect(self.graph_display_changed_cb)
+        self.graph_display_combobox.setCurrentIndex(0)  # default to NONE
+        glayout.addWidget(self.graph_display_combobox)
 
         msg = """
 Note: only for .mat cell plots (not .svg)
@@ -278,8 +279,8 @@ Note: only for .mat cell plots (not .svg)
         self.vis_tab.phenotype_cb()
         pass
 
-    def attachments_toggle_cb(self, bval):
-        self.vis_tab.attachments_checked_flag = bval
+    def graph_display_changed_cb(self):
+        self.vis_tab.graph_display_type = self.graph_display_combobox.currentText()
         self.vis_tab.update_plots()
     
 
