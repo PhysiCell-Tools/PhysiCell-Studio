3d126e79668614aa724fa9487cc838e5ffbcfadf
Randy Heiland <randy.heiland@gmail.com>
2026-06-14 11:40:13 -0400

    draft load samples

--- bin/studio.py
+++ bin/studio.py
@@ -96,7 +96,7 @@ def quit_cb():
     studio_app.quit()
 
 class PhysiCellXMLCreator(QWidget):
-    def __init__(self, config_file, studio_flag, skip_validate_flag, rules_flag, model3D_flag, tensor_flag, exec_file, nanohub_flag, galaxy_flag, is_movable_flag, pytest_flag, biwt_flag, parent = None):
+    def __init__(self, config_file, studio_flag, skip_validate_flag, rules_flag, model3D_flag, tensor_flag, exec_file, nanohub_flag, galaxy_flag, is_movable_flag, pytest_flag, biwt_flag, samples_flag, parent = None):
         super(PhysiCellXMLCreator, self).__init__(parent)
         QLocale.setDefault(QLocale(QLocale.English, QLocale.UnitedStates))
         if model3D_flag:
@@ -126,6 +126,7 @@ class PhysiCellXMLCreator(QWidget):
         self.ecm_flag = False 
         self.pytest_flag = pytest_flag 
         self.biwt_flag = biwt_flag
+        self.samples_flag = samples_flag
 
         self.rules_tab_index = None
 
@@ -523,6 +524,11 @@ PhysiCell Studio is provided "AS IS" without warranty of any kind. &nbsp; In no 
                 file_menu.addAction("Load user project", self.load_user_proj_cb)
 
                 file_menu.addSeparator()
+                if self.samples_flag:
+                    self.sample_models_menu = file_menu.addMenu("Load sample")
+                    self.sample_models_menu.addAction("zombies & villagers", self.load_zombies_villagers_cb)
+                    self.sample_models_menu.addAction("cancer,immune,drug", self.load_cancer_immune_drug_cb)
+
                 if PHYSIBOSS_MODELS_IMPORTED and self.physiboss_models_flag:
                     self.physiboss_models_menu = file_menu.addMenu("Load from PhysiBoSS-Models")
                     try:
@@ -543,6 +549,7 @@ PhysiCell Studio is provided "AS IS" without warranty of any kind. &nbsp; In no 
                                 )
                     except:
                         pass
+
 
         #-------------------------
         if self.model3D_flag:
@@ -870,6 +877,12 @@ PhysiCell Studio is provided "AS IS" without warranty of any kind. &nbsp; In no 
         except:
             print(f"--- Warning: cannot copy custom_modules/*")
 
+
+    #---------------------------------
+    def load_zombies_villagers_cb(self):
+        return
+    def load_cancer_immune_drug_cb(self):
+        return
 
     #---------------------------------
     def load_physiboss_model_cb(self, model_name, version=None):
@@ -1446,6 +1459,7 @@ def main():
     is_movable_flag = False
     pytest_flag = False
     biwt_flag = False
+    samples_flag = False
     try:
         parser = argparse.ArgumentParser(description='PhysiCell Studio.')
 
@@ -1459,6 +1473,7 @@ def main():
         parser.add_argument("-c ", "--config", type=str, help="config file (.xml)")
         parser.add_argument("-e ", "--exec", type=str, help="executable model")
         parser.add_argument("--bioinf_import","--biwt", dest="biwt_flag", help="display bioinformatics walkthrough tab on ICs tab", action="store_true")
+        parser.add_argument("--s","--samples", dest="samples_flag", help="menu for sample projects", action="store_true")
 
         if platform.system() == "Windows":
             exec_file = 'project.exe'
@@ -1523,6 +1538,8 @@ def main():
                 sys.exit()
         if args.biwt_flag:
             biwt_flag = True
+        if args.samples_flag:
+            samples_flag = True
     except:
         # print("Error parsing command line args.")
         sys.exit(-1)
@@ -1595,7 +1612,7 @@ def main():
             sys.exit(1)
             # print("Warning: Rules module not found.\n")
 
-    ex = PhysiCellXMLCreator(config_file, studio_flag, skip_validate_flag, rules_flag, model3D_flag, tensor_flag, exec_file, nanohub_flag, galaxy_flag, is_movable_flag, pytest_flag, biwt_flag)
+    ex = PhysiCellXMLCreator(config_file, studio_flag, skip_validate_flag, rules_flag, model3D_flag, tensor_flag, exec_file, nanohub_flag, galaxy_flag, is_movable_flag, pytest_flag, biwt_flag, samples_flag)
     # print("size=",ex.size())
 
     # -- Insanity. Trying/failing to force the proper display of (default) checkboxes
